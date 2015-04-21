c0195ab93a9b955607652237a92fd0902dd1b502
Michael Marschall <michael.a.m1990@gmail.com>
2015-04-21 23:17:13 +0200

    SlidingTab Implementation

--- app/src/main/java/com/example/bethere/InformationAdapter.java
+++ app/src/main/java/com/example/bethere/InformationAdapter.java
@@ -1,14 +1,12 @@
 package com.example.bethere;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Collections;
 import java.util.List;
@@ -64,9 +62,5 @@ public class InformationAdapter extends RecyclerView.Adapter<InformationAdapter.
 
 
         }
-
-
     }
-
-
 }
--- app/src/main/java/com/example/bethere/MainActivity.java
+++ app/src/main/java/com/example/bethere/MainActivity.java
@@ -1,17 +1,35 @@
 package com.example.bethere;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.bethere.Tab.SlidingTabLayout;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
+    private ViewPager mPager;
+    private SlidingTabLayout mTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +39,21 @@ public class MainActivity extends ActionBarActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-    NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
-            getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-        drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        mTabs.setDistributeEvenly(true);
+        mTabs.setCustomTabView(R.layout.custom_tab_view, R.id.tabText);
+
+        mTabs.setBackgroundColor(getResources().getColor(R.color.primaryColor));
+        mTabs.setSelectedIndicatorColors(getResources().getColor(R.color.accentColor));
+
+        mTabs.setViewPager(mPager);
+
     }
 
 
@@ -46,10 +76,70 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
-        if(id==R.id.add){
+        if (id == R.id.add) {
             startActivity(new Intent(this, AddNewsActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class MyFragment extends Fragment {
+
+        private TextView textView;
+
+        public static MyFragment getInstance(int position) {
+            MyFragment myFragment = new MyFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            myFragment.setArguments(args);
+            return myFragment;
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View layout = inflater.inflate(R.layout.fragment_news, container, false);
+            textView = (TextView) layout.findViewById(R.id.news);
+            Bundle bundle = getArguments();
+            if (bundle != null) {
+                textView.setText("The selected page is " + bundle.getInt("position"));
+            }
+
+            return layout;
+        }
+
+    }
+
+    class PagerAdapter extends FragmentPagerAdapter {
+
+        int icons[] = {R.drawable.ic_action_email, R.drawable.ic_action_event};
+        String[] tabText = getResources().getStringArray(R.array.tabs);
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+            tabText = getResources().getStringArray(R.array.tabs);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            MyFragment myFragment = MyFragment.getInstance(position);
+            return myFragment;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Drawable drawable=getResources().getDrawable(icons[position]);
+            drawable.setBounds(0,0,36,36);
+            ImageSpan imageSpan=new ImageSpan(drawable);
+            SpannableString spannableString=new SpannableString(" ");
+            spannableString.setSpan(imageSpan,0,spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            return spannableString;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
     }
 }
